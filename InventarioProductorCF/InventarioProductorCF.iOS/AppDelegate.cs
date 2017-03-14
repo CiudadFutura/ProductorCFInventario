9f4f740b0d2c23b6ad63807e58aa4426f91f6bf9
Diego <diegompaz@gmail.com>
2017-03-14 15:45:32 -0300

    Primera versión con lector de códigos.

--- InventarioProductorCF/InventarioProductorCF.iOS/AppDelegate.cs
+++ InventarioProductorCF/InventarioProductorCF.iOS/AppDelegate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace InventarioProductorCF.iOS
@@ -22,6 +18,7 @@ namespace InventarioProductorCF.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
--- InventarioProductorCF/InventarioProductorCF/MainPage.xaml.cs
+++ InventarioProductorCF/InventarioProductorCF/MainPage.xaml.cs
@@ -1,17 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
 
 namespace InventarioProductorCF
 {
     public partial class MainPage : ContentPage
     {
+        ZXingScannerPage scanPage;
         public MainPage()
         {
             InitializeComponent();
         }
+
+        async void AbrirPaginaEscaner(object sender, EventArgs args)
+        {
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) => {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopModalAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            // Navigate to our scanner page
+            await Navigation.PushModalAsync(scanPage);
+        }
+
+       
     }
 }
